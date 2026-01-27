5e079a0a352a203050cb206ed67ae2d51e170b53
singhmp968 <rahulsinghmp968@gmail.com>
2026-01-27 21:33:17 +0000

    completed restClient Need to recvise

--- orderService/orderService/src/main/java/com/cc/orderService/config/AppCongig.java
+++ orderService/orderService/src/main/java/com/cc/orderService/config/AppCongig.java
@@ -3,25 +3,32 @@ package com.cc.orderService.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppCongig {
-    @Bean(name = "simpleRestTemplate")
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
 
+    @Bean(name = "restClientImpl")
+    public RestClient restClientInstance(){
+        return RestClient.create();    }
 
-    @Bean(name = "customRestTemplate")
-    public RestTemplate customrestTemplate() {
-
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(3000);
-        factory.setReadTimeout(3000);
-        return new RestTemplate(factory);
-    }
+//    @Bean(name = "simpleRestTemplate")
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+//
+//
+//
+//    @Bean(name = "customRestTemplate")
+//    public RestTemplate customrestTemplate() {
+//
+//        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+//        factory.setConnectTimeout(3000);
+//        factory.setReadTimeout(3000);
+//        return new RestTemplate(factory);
+//    }
 
 
 }
--- orderService/orderService/src/main/java/com/cc/orderService/controller/OrderController.java
+++ orderService/orderService/src/main/java/com/cc/orderService/controller/OrderController.java
@@ -4,27 +4,66 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
     @Autowired
-    @Qualifier("customRestTemplate")
-    RestTemplate restTemplate;
+    @Qualifier("restClientImpl")
+    RestClient restClient;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<String> getOrderDetails(@PathVariable("id") Long orderId) {
+//    @GetMapping("/{id}")
+//    public ResponseEntity<String> getOrderDetails(@PathVariable("id") Long orderId) {
+//
+//        String responseObj = restClient.get()
+//                .uri("http://localhost:8082/products/" + orderId)
+//                .retrieve()
+//                // handling exceptions can be added here
+//                .onStatus(response ->{
+//                    if(response.getStatusCode().is4xxClientError()){
+//                        throw new RuntimeException("4xx error");
+//                    } else if (response.getStatusCode().is5xxServerError()) {
+//                        System.out.println("5xx error occurred while calling product service");
+//                    }
+//                    return false;
+//                })
+//                .body(String.class);
+//
+////        String response = restTemplate.getForObject("http://localhost:8082/products/" + orderId, String.class);
+//        System.out.println("Response from product service: " + responseObj);
+//        return ResponseEntity.ok("Order details for order id: " + orderId);
+//    }
 
 
-        String response = restTemplate.getForObject("http://localhost:8082/products/" + orderId, String.class);
-        System.out.println("Response from product service: " + response);
+@GetMapping("/{id}")
+public ResponseEntity<String> getOrderDetails(@PathVariable("id") Long orderId) {
+
+    String responseObj = restClient.get()
+            .uri("http://localhost:8082/products/" + orderId)
+            .exchange((request, response) -> {
+
+                if(response.getStatusCode().is4xxClientError()){
+                    throw new RuntimeException("4xx error");
+                } else if (response.getStatusCode().is5xxServerError()) {
+                    System.out.println("5xx error occurred while calling product service");
+                }
+                return StreamUtils.copyToString(response.getBody(), StandardCharsets.UTF_8);
+
+            });
+
+//        String response = restTemplate.getForObject("http://localhost:8082/products/" + orderId, String.class);
+        System.out.println("Response from product service: " + responseObj);
         return ResponseEntity.ok("Order details for order id: " + orderId);
-    }
+}
 
 }
